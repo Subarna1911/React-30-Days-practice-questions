03ab01b6537008139935359fc45014aa645d2e3e
subarna1911 <subarnablone09@gmail.com>
2025-05-01 20:38:54 +0530

    Day1 Like button

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LikeButton from './components/LikeButton'
+
+const App = () => {
+  return (
+  <>
+<LikeButton/>
+  </>
+  )
+}
+
+export default App
+
+
---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from 'react'
+
+const LikeButton = () => {
+
+    const[isClicked,setIsClicked] =useState(false);
+
+    const[isHovered,setIsHovered] =useState(false);
+
+    const handleClick =()=>{
+        setIsClicked(prevState=>!prevState)
+    }
+
+    const buttonStyle={
+        border:isHovered?'2px solid red':isClicked?'2px solid red':'2px solid gray',
+        color: isClicked?'white':isHovered ? 'red':'gray',
+        backgroundColor:isClicked ? 'red':'white',
+        borderRadius:'20px',
+        padding:'8px 12px',
+        cursor:'pointer',
+        transition:'all 0.2s  ease'
+    }
+
+    const heartStyle={
+        color:isClicked ? 'white':isHovered?'red' :'gray'
+    }
+
+  return (
+   <>
+   <div style={{display:"flex",justifyContent:"center",alignItems:"center",marginTop:"50"}}>
+    <button style={buttonStyle} onClick={handleClick}
+    onMouseEnter={()=> setIsHovered(true)}
+    onMouseLeave={()=>  setIsHovered(false)}
+    
+    > <span style={heartStyle}>{isClicked? '🤍' : '❤'}</span>Like</button>
+   </div>
+   </>
+  )
+}
+
+export default LikeButton
